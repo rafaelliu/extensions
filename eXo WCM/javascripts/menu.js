036c5202ea0846a68202e2725e97c93cb628d053
rafaelliu <rafaelliu@gmail.com>
2011-06-09 08:41:47 -0700

    Limpeza de código

--- eXo WCM/javascripts/menu.js
+++ eXo WCM/javascripts/menu.js
@@ -1,11 +1,3 @@
-// JavaScript Document
-var size = 65;
-
-function accessibilidade(i) {
-	size = Math.max(60, Math.min(85, (size + i*5)));
-	$j("body").css("font-size", size + "%");
-}
-
 var nodes = eXo.env.portal.navigations[0].nodes;
 var selectedNodeUri = eXo.env.portal.selectedNodeUri;
 var baseUri = eXo.env.portal.context + '/' + eXo.env.portal.accessMode + '/' + eXo.env.portal.portalName + '/';
@@ -33,9 +25,3 @@ var markup = createMarkup(nodes, true);
 $j(document).ready(function(){
 	$j("#menu").append(markup);
 });
-
-$j(function(p){
-
-	$j(".search").doSearch({message: "Busca no menu"});
-
-}); 
